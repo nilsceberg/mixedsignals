7dc6a1d36ef689e4806fed6b014d4094200c9117
Nils Ceberg <nils.ceberg@gmail.com>
2020-03-31 22:13:05 +0200

    Make visualizer more stable

--- src/nodes/GraphNode.tsx
+++ src/nodes/GraphNode.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Scatter } from "react-chartjs-2";
 
@@ -8,23 +8,53 @@ import { observer } from "mobx-react";
 import { NodeProps } from "./Node";
 import { Graph } from "../processing/digital/Graph";
 
-export const GraphNode = observer((props: NodeProps<Graph>) => {
-	return <Node name="Visualizer" io={[{direction: "input", name: "buffer", type: SignalType.Buffer}]} {...props}>
-			<Scatter data={{
-				datasets: [{
-					label: "signal",
-					fill: true,
-					data: props.process.samples.map((y, n) => ({x: n - props.process.samples.length, y: y})),
-				}]
-			}} options={{
-				scales: {
-					xAxes: [{
-						ticks: {
-							min: -props.process.samples.length,
-							max: 0,
-						}
+@observer
+export class GraphNode extends React.Component<NodeProps<Graph>, { largest: number }> {
+	constructor(props: NodeProps<Graph>) {
+		super(props);
+		this.state = {
+			largest: 0,
+		};
+	}
+
+	componentDidUpdate() {
+		let largestCandidate = 0;
+		for (const y of this.props.process.samples) {
+			if (Math.abs(y) > largestCandidate) {
+				largestCandidate = Math.abs(y);
+			}
+		}
+		if (largestCandidate > this.state.largest) {
+			this.setState({
+				largest: largestCandidate
+			});
+		}
+	}
+
+	render() {
+		return <Node name="Visualizer" io={[{direction: "input", name: "buffer", type: SignalType.Buffer}]} {...this.props}>
+				<Scatter data={{
+					datasets: [{
+						label: "signal",
+						fill: true,
+						data: this.props.process.samples.map((y, n) => ({x: n - this.props.process.samples.length, y: y})),
 					}]
-				}
-			}}/>
-		</Node>;
-});
+				}} options={{
+					scales: {
+						xAxes: [{
+							ticks: {
+								min: -this.props.process.samples.length,
+								max: 0,
+							}
+						}],
+						yAxes: [{
+							ticks: {
+								min: -this.state.largest,
+								max: this.state.largest,
+							}
+						}]
+					}
+				}}/>
+			</Node>;
+	}
+}
