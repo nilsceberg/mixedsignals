e0816000e352db594d9aaa8b564ba5c103f63c8f
Nils Ceberg <nils.ceberg@gmail.com>
2020-03-31 21:20:39 +0200

    Rename sampler to AD converter in UI

--- src/nodes/SamplerNode.tsx
+++ src/nodes/SamplerNode.tsx
@@ -8,7 +8,7 @@ import { NodeProps } from "./Node";
 import { SignalType } from "../editor/Types";
 
 export const SamplerNode = observer((props: NodeProps<Sampler>) => {
-	return <Node name="Sampler" io={[{direction: "input", name: "analog", type: SignalType.Analog}, {direction: "output", name: "digital", type: SignalType.Digital}]} {...props}>
+	return <Node name="AD converter" io={[{direction: "input", name: "analog", type: SignalType.Analog}, {direction: "output", name: "digital", type: SignalType.Digital}]} {...props}>
 		<TextField value={props.process.frequency} onChange={event => props.process.frequency = Number.parseFloat(event.target.value)} size="small" variant="outlined" label="Frequency (Hz)" type="number"/><br/>
 		<TextField disabled value={props.process.lastSample} size="small" variant="outlined" label="Last sample" type="number"/><br/>
 	</Node>;
