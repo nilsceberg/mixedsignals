09c996ef3c7acaec441d514622c2081357f5d904
Nils Ceberg <nils.ceberg@gmail.com>
2020-04-02 03:00:12 +0200

    Fix placement offset

--- src/editor/Blueprint.tsx
+++ src/editor/Blueprint.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { Button, Theme } from "@material-ui/core";
-import Draggable from "react-draggable";
+import { Button, Theme, ButtonBaseActions } from "@material-ui/core";
+import Draggable, { DraggableData, DraggableEvent } from "react-draggable";
 import { makeStyles, createStyles } from "@material-ui/styles";
 import { SystemConstructor } from "../processing/System";
 
@@ -27,11 +27,19 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
 
 export const Blueprint = (props: BlueprintProps) => {
 	const classes = useStyles();
+	const container = React.useRef<HTMLDivElement>(null);
 	const onPlace = props.onPlace || (() => {});
+
+	const onDragStop = (e: DraggableEvent, data: DraggableData) => {
+		if (container.current === null) return;
+		const rect = container.current.getBoundingClientRect();
+		onPlace(props.system, props.node, { x: data.x + rect.left, y: data.y + rect.top });
+	};
+
 	return (
-		<div className={classes.container}>
+		<div className={classes.container} ref={container}>
 			<Button fullWidth className={classes.ghost} color="secondary" variant="outlined" disabled>{props.label}</Button>
-			<Draggable position={{x: 0, y: 0}} onStop={(e, data) => onPlace(props.system, props.node, { x: data.x, y: data.y })}>
+			<Draggable position={{x: 0, y: 0}} onStop={onDragStop}>
 				<Button fullWidth className={classes.button} color="secondary" variant="contained">{props.label}</Button>
 			</Draggable>
 		</div>
